30c2ad631bd2d61dd0b1aa174b5378b01e78c516
@9-lives <911120823@qq.com>
2019-01-12 23:44:36 +0800

    chore(pakcage.json): upgrades dependencies
    feat: new util getScrollBarHt、isIntoView

--- src/utils/func/throttle.js
+++ src/utils/func/throttle.js
@@ -1,19 +1,20 @@
 /**
- * 节流。兼容到IE10
+ * 节流。兼容到 IE 10
  * @param {Function} callback 回调方法
  * @return {Function} 去抖后的回调方法
  */
 export function throttle ({ callback }) {
   let isRunning // 运行时标识
+  let rqFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame
 
-  return function (...args) {
+  return rqFrame ? function (...args) {
     if (!isRunning) {
-      window.requestAnimationFrame(timestamp => {
+      rqFrame(timestamp => {
         Reflect.apply(callback, this, args)
         isRunning = false
       })
     }
 
     isRunning = true
-  }
+  } : callback
 }
